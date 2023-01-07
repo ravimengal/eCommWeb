b10cb9d91490435514163cd9dd94a17f923a32b7
Ravindra Mengal <45294976+ravimengal@users.noreply.github.com>
2023-01-07 23:16:38 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router , Routes,Route} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import ContactUs from './components/ContactUs';
+import Product from './components/Product';
+import Cart from './components/Cart';
+import ProductDetail from './components/ProductDetail';
+import ErrorPage from './components/ErrorPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/contactus' element={<ContactUs/>} />
+      <Route path='/product' element={<Product/>}/>
+      <Route path='/productDetail/:id' element={<ProductDetail/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='*' element={<ErrorPage/>}/>
+
+    </Routes>
+   </Router>
   );
 }
 
